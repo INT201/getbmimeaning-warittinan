b64ba6ef602e08ae49dca19761fe5b3c4d164f09
warittinan <warittinan.kh@mail.kmutt.ac.th>
2022-08-31 10:32:01 +0700

    test1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,17 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  calBMI=weight/(height*height)
+  return calBMI
 }
-function getBMIMeaning(weight, height) {
-  //code here
+function getBMIMeaning(calculateBMI) {
+  if (calculateBMI <18.5) {
+     return 'Underweight'
+  }else if (calculateBMI >25.0) {
+    return "Overweight"
+  } else{
+    return "Normal weight"
+  } 
 }
 module.exports = getBMIMeaning
+
